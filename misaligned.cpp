537499d7e4a5a64b67c90a9a46c62258b35ce3e2
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-21 18:47:26 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <assert.h>
 
+int colour_index_checker;
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    const char* major_col, minor_col;
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            major_col, minor_col = majorMinorColourFinder(i,j);
+            std::cout << i * 5 + j << " | " << major_col << " | " << minor_col << "\n";
         }
     }
     return i * j;
 }
 
+const char* majorMinorColourFinder(int major_index, int Minor_index)
+{
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    major_color = majorColor[major_index];
+    minor_color = minorColor[minor_index];
+    colour_index_checker = major_index * minor_index + 1
+    return major_color, minor_color;
+}
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(result == colour_index_checker);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
