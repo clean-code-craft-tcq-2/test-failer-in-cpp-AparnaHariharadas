ae0ee14acf6c73b41f4e26bac6ca5ad60db2c0d2
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-21 19:07:35 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,24 +3,22 @@
 
 int colour_index_checker;
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-const char* majorMinorColourFinder(int major_index, int minor_index)
+void majorMinorColourIndexFinder(int major_index, int minor_index)
 {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    const char* major_color = majorColor[major_index];
-    const char* minor_color =minorColor[minor_index];
     colour_index_checker = major_index * minor_index + 1;
-    return (major_color, minor_color);
+  
 }
 
 int printColorMap() {
-   //char major_col, minor_col;
+  
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            const char* major_col, minor_col = majorMinorColourFinder(i,j);
-            std::cout << i * 5 + j << " | " << major_col << " | " << minor_col << "\n";
+            majorMinorColourIndexFinder(i,j);
+            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << majorColor[j] << "\n";
         }
     }
     return i * j;
