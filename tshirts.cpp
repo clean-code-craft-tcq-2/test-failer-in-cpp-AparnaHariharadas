acce8d4e77ae0822c2f6dd99c893478905066155
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-28 17:36:08 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,10 +5,13 @@ char size(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms >= 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms >= 42) {
         sizeName = 'L';
+    }
+    else {
+        sizeName = 'U';
     }
     return sizeName;
 }
