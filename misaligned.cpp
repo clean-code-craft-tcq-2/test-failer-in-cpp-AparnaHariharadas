f1d564b36f4e0a8cd61667ccb0bc4f752ddb8aba
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-01-28 17:35:00 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -2,31 +2,31 @@
 #include <assert.h>
 
 int colour_index_checker;
+#define COLORMULTIPLIER 5
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 void majorMinorColourIndexFinder(int major_index, int minor_index)
 {
-    colour_index_checker = major_index * minor_index + 1;
+    colour_index_checker = (major_index *COLORMULTIPLIER + minor_index) + 1;
   
 }
 
 int printColorMap() {
   
     int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
+    for(i = 0; i < COLORMULTIPLIER; i++) {
+        for(j = 0; j < COLORMULTIPLIER; j++) {
             majorMinorColourIndexFinder(i,j);
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << majorColor[j] << "\n";
+            std::cout <<std::setw(3)<<std::left<< (i * COLORMULTIPLIER + j) + 1 <<std::left << std::setw(4) << " | " << std::left << std::setw(6) << majorColor[i] << std::left << std::setw(4) << " | " << majorColor[j] << std::left << std::setw(4) << "\n";
         }
     }
-    return i * j;
+    return i *COLORMULTIPLIER + j + 1;
 }
 
 int main() {
     int result = printColorMap();
-    assert(result == 25);
     assert(result == colour_index_checker);
     std::cout << "All is well (maybe!)\n";
     return 0;
