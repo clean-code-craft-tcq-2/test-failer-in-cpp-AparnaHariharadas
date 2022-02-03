71cb2d7ba8770990943e6e8da17bed0030699e03
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 10:46:21 +0530

    Create testtshirts.cpp

---
+++ testtshirts.cpp
@@ -0,0 +1,14 @@
+#include "tshirts.h"
+
+
+void testTshirtSize(int cms , char expectedSize)
+    assert(size(cms) == expectedSize);
+    std::cout << "All is well (maybe!)\n";
+
+int main() {
+    testTshirtSize(37 , 'S');
+    testTshirtSize(40 , 'M');
+    testTshirtSize(43 , 'L');
+    testTshirtSize(38 , 'M');
+    return 0;
+}
