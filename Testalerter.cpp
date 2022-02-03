e9853f8fcc4e2efbc6d6c06bed408a48b55050fa
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 11:28:10 +0530

    Update Testalerter.cpp

--- Testalerter.cpp
+++ Testalerter.cpp
@@ -1,15 +1,3 @@
-#include "alerter.h"
+//#include "alerter.h"
 
-void testAlerter(void){
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    alertInCelcius(480.5);
-    assert(alertFailureCount == checkalertFailureCount);
-    std::cout << alertFailureCount << " alerts failed.\n";
-    std::cout << "All is well (maybe!)\n";
-}
 
-int main() {
-    testAlerter();
-    return 0;
-}
