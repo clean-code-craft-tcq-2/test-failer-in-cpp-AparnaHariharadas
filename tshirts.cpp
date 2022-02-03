9fc71cc1151a151f3093bcb6e61c40328ab90a6c
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 11:22:17 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -14,3 +14,11 @@ char size(int cms) {
     }
     return sizeName;
 }
+
+int main() {
+    testTshirtSize(37 , 'S');
+    testTshirtSize(40 , 'M');
+    testTshirtSize(43 , 'L');
+    testTshirtSize(38 , 'M');
+    return 0;
+}
