ef265ca00ef6848cb95cb7552ea37941639845c0
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 10:55:31 +0530

    Create misaligned.h

---
+++ misaligned.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <assert.h>
+#include <iomanip>
+
+using namespace std;
+
+int colour_index_checker;
+#define COLORMULTIPLIER 5
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int printColorMap();
