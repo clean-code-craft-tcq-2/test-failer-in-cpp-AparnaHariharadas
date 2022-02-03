e5f83d00eb665a8dcae3b10713b619893316da06
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 11:28:23 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -27,3 +27,16 @@ void alertInCelcius(float farenheit) {
     }
 }
 
+void testAlerter(void){
+    alertInCelcius(400.5);
+    alertInCelcius(303.6);
+    alertInCelcius(480.5);
+    assert(alertFailureCount == checkalertFailureCount);
+    std::cout << alertFailureCount << " alerts failed.\n";
+    std::cout << "All is well (maybe!)\n";
+}
+
+int main() {
+    testAlerter();
+    return 0;
+}
