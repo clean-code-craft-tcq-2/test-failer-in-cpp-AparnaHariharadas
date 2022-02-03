2ff31a9fe9603a93266c6dc3b4643edd76564a3f
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-02-03 11:05:59 +0530

    Update testmisaligned.cpp

--- testmisaligned.cpp
+++ testmisaligned.cpp
@@ -1,13 +1,2 @@
-#include "misaligned.h"
+//#include "misaligned.h"
 
-
-void testPrintColorMap(){
-    int result = printColorMap();
-    assert(result == colour_index_checker);
-    cout << "All is well (maybe!)\n";
-}
-  
-int main() {
-    testPrintColorMap();
-    return 0;
-}
